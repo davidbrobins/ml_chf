e6fe917ade06b4b83a6a9c68d8e3388d4bb14556
davidbrobins <davidbenjaminrobinson@gmail.com>
2022-08-01 15:50:29 -0400

    Added config parsing and training data IO models

---
+++ config.py
@@ -0,0 +1,78 @@
+# Module to handle reading in needed parameters from a configuration file
+
+# Import configuration file parser
+import configparser
+
+def read_config_file(model_dir):
+    '''
+    Function to read in a config file for a given model and return parameters needed elsewhere.
+    Input:
+    model_dir (str): Path to the directory containing the configuration file (should be named 'config.ini').
+    Output:
+    data_path (str): Path to directory containing the training data.
+    random_seed (int): Random seed (for reproducibility)
+    alpha_vals (list): List of values of alpha parameter (which indexes training data) to use in training.
+    target (str): String labelling the target for XGBoost to predict.
+    output (str): String specifying whether model is for CF or HF.
+    metallicity (int): Integer value (0, 1, or 2) at which output is evaluated (to get the target).
+    restricted_params (dict): Dictionary containing training data parameters restricted to one value, and those values.
+    features (list): List of parameters to use as features in the XGBoost model.
+    grid_search_params (dict): Dictionary containing arrays of values for the hyperparameters to grid search through.
+    '''
+
+    # Set up configuration file parsing
+    config = configparser.ConfigParser()
+    # Read in the configuration file (named 'config.ini').
+    config.read(model_dir + '/config.ini')
+
+    # Get the path to the training data
+    data_path = config['IO']['training_data_path']
+
+    # Get random seed
+    random_seed = config['IO']['random_seed']
+    
+    # Get list of alpha values to read in from training data
+    # Check if alpha is given as a restricted parameter
+    if 'alpha' in config['restricted_input_params']:
+        # If so, get its value
+        alpha_vals = [float(config['restricted_input_params']['alpha'])]
+    else:
+        # Otherwise, just use all 7 possible values
+        alpha_vals = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0] 
+    
+    # Get string to label target column with
+    target = config['target_params']['output'] + '_Z_' + config['target_params']['Z']
+    # Get string of output type (CF or HF)
+    output = config['target_params']['output']
+    # Get numerical value of metallicity
+    metallicity = int(config['target_params']['Z'])
+
+    # Get list of features for the XGBoost model
+    features = [key for key in config['features']]
+    
+    # Get dictionary of training data parameters to restrict, and the values to restrict them to
+    # Set up dictionary
+    restricted_params = {}
+    # Loop through names of parameters to restrict
+    for key in config['restricted_input_params']:
+        # Set the key to the appropriate value
+        restricted_params[key] = config['restricted_input_params'][key]
+
+    # Get dictionary of hyperparameters to include in grid search, and the arrays of values to consider for them
+    # Set up dictionary
+    grid_search_params = {}
+    # Loop through names of hyperparameters in the grid search
+    for key in config['grid_search_params']:
+        # Some hyperparameters need integer values
+        if key == 'max_depth' or key == 'lambda' or key == 'alpha' or key == 'n_estimators':
+            # Get list of hyperparameter values as integers
+            grid_search_params[key] = [int(x.strip()) for x in config['grid_search_params'][key].split(',')]
+        # Otherwise, float values are fine
+        else:
+            # Get list of hyperparameter values as floats
+            grid_search_params[key] = [float(x.strip()) for x in config['grid_search_params'][key].split(',')]
+    # Add a sampling method (not set in config file)
+    grid_search_params['sampling_method'] = ['uniform']
+
+    return data_path, random_seed, alpha_vals, target, output, metallicity, features, restricted_params, grid_search_params
+
---
+++ io.py
@@ -0,0 +1,64 @@
+# Module handling reading in the needed subset of training data as a dataframe
+
+import pandas as pd # Pandas for dataframe handling
+import numpy as np # Numpy for math
+
+def get_training_data(data_path, alpha_vals, target, output, metallicity, restricted_params):
+    '''
+    Function to read in the needed susbset of training data, given relevant parameters from the config file.
+    Input:
+    data_path (str): Path to the directory containing the training data.
+    alpha_vals (list): Values of alpha parameter to include.
+    target (str): The name of the target column
+    output (str): Which of CF or HF is to be predicted.
+    metallicity (int): Value of metallicity (0, 1, or 2 times solar) at which output is evaluated to get target.
+    restricted_params (dict): Columns of training data to be restricted to a single value, and those values.
+    Output:
+    data_df (dataframe): The desired subset of the training data.
+    '''
+
+    # Photoionization rate data column names
+    prate_cols = ['log10(f_q)', 'log10(tau_0)', 'P_LW', 'P_HI', 'P_HeI', 
+                  'P_HeII', 'P_CVI', 'P_Al13', 'P_Fe26', 'P_CI', 'P_C04',
+                  'P_C05', 'P_O06', 'P_O08', 'P_F09', 'P_Ne10', 'P_Na11',
+                  'P_Mg12', 'P_Si14', 'P_S16', 'P_Ar18', 'P_Ca20'] 
+    # CHF data column names
+    chf_cols = ['log10(n_b) [cm^{-3}]', 'log10(T) [K]', 'log10(J_0/n_b/J_{MW})', 
+                'log10(f_q)', 'log10(tau_0)', 'CF_0', 'CF_1', 'CF_2', 'HF_0', 
+                'HF_1', 'HF_2'] 
+    # Create a blank array to store the dataframes for each alpha
+    alpha_dfs = {}
+    
+    # Loop through alpha values
+    for alpha in alpha_vals:
+        # Read in photoionization rate data at this alpha value
+        p_rates = pd.read_csv(data_path + '/p_rate_data/prates_'+str(alpha)+'.dat', sep= '\s+', names = prate_cols)
+        # Read in CHF data at this alpha value
+        chf = pd.read_csv(data_path + '/chf_data/d'+str(alpha)+'.res', skiprows = 2, sep='\s+',
+                          usecols = [0, 1, 2, 3, 4, 11, 12, 13, 16, 17, 18], names = chf_cols)
+        
+        # Calculate target
+        chf[target] = sum([chf[output + '_' +str(i)] * (2**i) for i in range(metallicity+1)])
+        
+        # Merge the two dataframes on the matching columns (fq, tau0)
+        merged = chf.merge(p_rates, on = ['log10(f_q)', 'log10(tau_0)'])
+        # Now scale each photoionization rate by J0 (first two entries of prate_cols are NOT photoionization rates)
+        for col in prate_cols[2:]:
+            merged[col] = merged[col] * 10 ** merged['log10(J_0/n_b/J_{MW})']
+        
+        # Add a column with the alpha value
+        merged['alpha'] = alpha
+
+        # Restrict any input parameters specified in the config file
+        # Loop through parameters to restrict
+        for key in restricted_params: 
+            # Restrict them to the specified values
+            merged = merged.loc[(merged[key] == restricted_params[key])]
+
+        # Put the merged dataframe in the dictionary
+        alpha_dfs[alpha] = merged
+
+    # Merge dataframes for all the alpha values
+    data_df = pd.concat([alpha_dfs[alpha] for alpha in alpha_vals])
+
+    return data_df
